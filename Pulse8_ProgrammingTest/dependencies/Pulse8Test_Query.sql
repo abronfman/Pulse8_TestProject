85d48af09e1effddac550e36c4e4a419fcd57b25
Alex Bronfman <alex.bronfman@gmail.com>
2018-05-01 21:55:19 -0400

    Adjusting query and sproc based on feedback

--- Pulse8_ProgrammingTest/dependencies/Pulse8Test_Query.sql
+++ Pulse8_ProgrammingTest/dependencies/Pulse8Test_Query.sql
@@ -1,5 +1,4 @@
 /*
-
 Write a query to return a list of all members and all of their corresponding categories:
 a. Please include the following fields: Member ID, First Name, Last Name, Most Severe Diagnosis ID, Most Severe Diagnosis Description, Category ID, Category Description, Category Score and Is Most Severe Category.
 b. Most Severe Diagnosis ID and Description should be the diagnosis with the lowest Diagnosis ID present for each Member’s Category.
@@ -8,39 +7,37 @@ d. Members without Diagnosis or Categories should be included in the result set.
 
 */
 
-
 use Pulse8TestDB
 Go
 
-;WITH MostSevereMemberDiagnosis as (
-	Select MemberId, MIN(DiagnosisID) as LowestDiagnosisID
-	from MemberDiagnosis
-	Group By MemberID
-) 
-Select m.MemberID,
-	m.FirstName,
-	m.LastName,
-	severity.LowestDiagnosisID as MostSevereDiagnosisID,
-	d.DiagnosisDescription as MostSevereDiagnosisDescription,
-	cat.DiagnosisCategoryID as CategoryID,
+Select m.MemberID, 
+	m.FirstName, 
+	m.LastName, 
+	min(md.DiagnosisID) as MostSevereDiagnosisID,
+	(	
+		Select Diagnosis.DiagnosisDescription 
+		from Diagnosis 
+		where Diagnosis.DiagnosisID = min(md.DiagnosisId)
+	) as MostSevereDiagnosisDescription, 
+	map.DiagnosisCategoryID as CategoryID,
 	cat.CategoryDescription,
 	cat.CategoryScore,
-	case 
-		when severity.LowestDiagnosisID is not null 
-			then 1
-		when severity.LowestDiagnosisID is null AND cat.DiagnosisCategoryID is null	
-			then 1
-		else 0
-	end as IsMostSevereCategory
+		case 
+			when severity.mostSevereCategory is not null then 1
+			when severity.mostSevereCategory is null AND map.DiagnosisCategoryID is null then 1
+			else 0
+		end as IsMostSevere
 from Member m
-left outer join MemberDiagnosis md on m.MemberID = md.MemberID
-left outer join DiagnosisCategoryMap map on map.DiagnosisID = md.DiagnosisID
+left outer join MemberDiagnosis md on m.MemberID= md.MemberID
+left outer join DiagnosisCategoryMap map on md.DiagnosisID = map.DiagnosisID
 left outer join DiagnosisCategory cat on map.DiagnosisCategoryID = cat.DiagnosisCategoryID
-left outer join MostSevereMemberDiagnosis severity on m.MemberID = severity.MemberID
-	and md.DiagnosisID = severity.LowestDiagnosisID
-left outer join Diagnosis d on severity.LowestDiagnosisID = d.DiagnosisID
-where m.MemberID = 1
-Order by m.MemberID
-
-exec member_getDiagnosisAndSeverity @MemberId = 1
-
+left outer join (
+	select m.MemberID, min(map.DiagnosisCategoryID) mostSevereCategory
+	from MemberDiagnosis m
+	join DiagnosisCategoryMap map on m.DiagnosisID = map.DiagnosisID
+	group by m.MemberID
+) as severity 
+	on m.MemberID = severity.MemberID	
+		AND cat.DiagnosisCategoryID = severity.mostSevereCategory
+group by m.MemberID,m.FirstName, m.LastName, map.DiagnosisCategoryID, 
+	cat.CategoryDescription, cat.CategoryScore, severity.mostSevereCategory
